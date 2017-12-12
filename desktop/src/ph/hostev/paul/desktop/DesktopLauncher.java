d02a7a15175e1501ac13878cef905a9d10ad273c
paulhostev <pg@rention.com>
2017-12-13 00:20:54 +0200

    init project

---
+++ desktop/src/ph/hostev/paul/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ph.hostev.paul.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ph.hostev.paul.MousetrapStarter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MousetrapStarter(), config);
+	}
+}
